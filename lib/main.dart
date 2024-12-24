5afb5fe8c8fdd66497b8262db577c6ccf87c9cce
RaninduAmarasinghe <raninduamarasinghe@gmail.com>
2024-12-24 20:02:36 +0530

    image

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,46 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
+      title: 'Bus Buddy',
+      home: const HomePage(), // Set HomePage as the initial screen
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       appBar: AppBar(
-        title: Text("BusBuddy"),
+        title: const Text("BusBuddy"),
+        leading: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Image.asset(
+            'assets/images/bus-station.png', // Ensure the image exists in this path
+            fit: BoxFit.contain,
+          ),
+        ),
         actions: [
           IconButton(
-            icon: Icon(Icons.person),
-            onPressed: () {},
-          )
+            icon: const Icon(Icons.person),
+            onPressed: () {
+              // Add functionality here if needed
+            },
+          ),
         ],
       ),
-    ));
+      body: const Center(),
+    );
   }
 }
